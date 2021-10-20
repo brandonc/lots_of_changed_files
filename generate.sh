c7ab712e1bdc771b4a331147d30fd4d00d22cff7
Brandon Croft <brandon.croft@gmail.com>
2021-10-20 12:10:13 -0600

    fix

--- generate.sh
+++ generate.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-for i in $(seq "$1");
+for i in $(seq 0 "$1");
 do
   mkdir "generated${i}"
 done
 
-for i in $(seq "$1");
+for i in $(seq 0 "$1");
 do
   uuidgen > "generated${i}/addition_${i}_of_${1}.txt"
 done
