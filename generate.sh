75d69d07bed25d294163877843c4bdd02f818f41
Brandon Croft <brandon.croft@gmail.com>
2021-10-20 12:04:50 -0600

    inital state

--- generate.sh
+++ generate.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-mkdir "generated"
 for i in $(seq "$1");
 do
-  uuidgen > "generated/addition_${i}_of_${1}.txt"
+  mkdir "generated${i}"
+done
+
+for i in $(seq "$1");
+do
+  uuidgen > "generated${i}/addition_${i}_of_${1}.txt"
 done
